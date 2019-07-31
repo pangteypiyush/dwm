47075b5018e5a11973d20556fae05d1c335b232d
Piyush Pangtey <gokuvsvegita@gmail.com>
2019-07-31 15:39:31 +0530

    [Update] shiftview keybindings
    Signed-off-by: Piyush Pangtey <gokuvsvegita@gmail.com>

--- config.h
+++ config.h
@@ -139,6 +139,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_Tab,    shiftview,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Tab,    shiftview,      {.i = -1 } },
 	{ MODKEY,                       XK_j,      togglealttag,   {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
