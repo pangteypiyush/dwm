1701562d376ed15a9116d391036f41f948b5cbec
Piyush Pangtey <gokuvsvegita@gmail.com>
2019-07-31 13:51:52 +0530

    [Update] config: topbar
    Signed-off-by: Piyush Pangtey <gokuvsvegita@gmail.com>

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 6;        /* gap pixel between windows */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "SauceCodePro Nerd Font:size=11" };
 static const char dmenufont[]       = "SauceCodePro Nerd Font:size=11";
 
